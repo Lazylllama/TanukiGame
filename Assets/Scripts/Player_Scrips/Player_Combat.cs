62fc422837833d1a9ba9f8708db5a1588a2e96e1
Raymondaderman <raymond.aderman@gmail.com>
2026-09-03 13:03:08 +0200

    feat: player combat

--- Assets/Scripts/Player_Scrips/Player_Combat.cs
+++ Assets/Scripts/Player_Scrips/Player_Combat.cs
@@ -1,16 +1,38 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player_Combat : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+public class Player_Combat : MonoBehaviour {
+	[Header("Combat Settings")]
+	[SerializeField] private float slashRadius;
+	[SerializeField] private int slashDamage;
+	[SerializeField] private int slashCooldown;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[Header("Drag and Drop")]
+	[SerializeField] private Transform slashPoint;
+
+	private float slashTimer;
+
+
+	private LayerMask enemyLayer;
+
+	private void Awake() {
+		enemyLayer = LayerMask.GetMask("Enemy");
+	}
+
+	private void SlashAttack() {
+		var enemies = Physics2D.OverlapCircleAll(slashPoint.position, slashRadius, enemyLayer);
+
+		if (enemies == null) return;
+		foreach (var enemy in enemies) {
+		}
+	}
+
+
+	private void OnSlash(InputValue value) {
+		if (!value.isPressed || !(slashTimer <= 0f)) return;
+
+		slashTimer = slashCooldown;
+		SlashAttack();
+	}
 }
