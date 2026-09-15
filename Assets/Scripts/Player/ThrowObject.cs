a637530def05f8ff86702fda6482d930de0ece81
Simon Klövdahl <me@lazyllama.xyz>
2026-09-15 22:43:22 +0200

    feat: stuff

--- Assets/Scripts/Player/ThrowObject.cs
+++ Assets/Scripts/Player/ThrowObject.cs
@@ -3,20 +3,22 @@ using Enemy;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ThrowObject : MonoBehaviour {
-	[Header("ThrowObject Settings")]
-	[SerializeField] private float throwDamage;
-	[SerializeField] private float throwKnockbackForce;
-	[SerializeField] private float throwKnockbackLength;
+namespace Player {
+	public class ThrowObject : MonoBehaviour {
+		[Header("ThrowObject Settings")]
+		[SerializeField] private float throwDamage;
+		[SerializeField] private float throwKnockbackForce;
+		[SerializeField] private float throwKnockbackLength;
 
-	private void OnTriggerEnter2D(Collider2D enemy) {
-		if (enemy.gameObject.layer == LayerMask.GetMask("Enemy")) {
-			Debug.Log("Enemy hit with ranged object");
-			enemy.GetComponent<EnemyHealth>().ChangeHealth(-throwDamage);
+		private void OnTriggerEnter2D(Collider2D enemy) {
+			if (enemy.gameObject.layer == LayerMask.GetMask("Enemy")) {
+				Debug.Log("Enemy hit with ranged object");
+				enemy.GetComponent<EnemyHealth>().ChangeHealth(-throwDamage);
 
-			var knockbackDirection = Mathf.Sign(enemy.transform.position.x - transform.position.x);
-			StartCoroutine(Lib.Combat.PreformedKnockback(enemy.GetComponent<Rigidbody2D>(), knockbackDirection,
-			                                             throwKnockbackForce, throwKnockbackLength));
+				var knockbackDirection = Mathf.Sign(enemy.transform.position.x - transform.position.x);
+				StartCoroutine(Lib.Combat.PreformedKnockback(enemy.GetComponent<Rigidbody2D>(), knockbackDirection,
+				                                             throwKnockbackForce, throwKnockbackLength));
+			}
 		}
 	}
 }
