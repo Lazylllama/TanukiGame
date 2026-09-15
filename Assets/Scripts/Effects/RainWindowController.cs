8ec6862b22ed961975f6dfb843dbc268b0b59cf0
Thino Ivarsson <thinoivarsson7@gmail.com>
2026-09-16 00:42:12 +0200

    feat: improved rain

---
+++ Assets/Scripts/Effects/RainWindowController.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class RainWindowController : MonoBehaviour
+{
+	#region Fields
+	
+	//? Settings 
+
+	[SerializeField] private float fallSpeed = 30, windSpeed;
+	private                  float oldFallSpeed, oldWindSpeed;
+
+	//? Refs
+
+	[SerializeField] private ParticleSystem rainSystem;
+	[SerializeField] private Transform      rainArea;
+
+	#endregion
+
+	#region Unity Functions
+
+	private void Start() {
+		PositionSystem();
+	}
+
+	private void Update() {
+		if (!Mathf.Approximately(fallSpeed, oldFallSpeed) || !Mathf.Approximately(windSpeed, oldWindSpeed)) {
+			PositionSystem();
+			oldFallSpeed = fallSpeed;
+			oldWindSpeed = windSpeed;
+		}
+	}
+	#endregion
+
+	#region Custom Functions
+
+	private void PositionSystem() {
+		var center = rainArea.position;
+		var scaleX = rainArea.localScale.x;
+		var scaleY = rainArea.localScale.y;
+		var perimeter = Mathf.Sqrt(scaleX * scaleX + scaleY * scaleY);
+		var offset = new Vector2(windSpeed, -fallSpeed);
+		offset.Normalize();
+		offset *= -perimeter / 2f;
+		
+		var shape = rainSystem.shape;
+		var main  = rainSystem.main;
+		shape.position = new Vector3(center.x + offset.x, center.y + offset.y, 0);
+		shape.scale    = new Vector3(1,                   perimeter,           1);
+		main.startSpeed
+		main.startSpeed = Mathf.Sqrt(windSpeed * windSpeed + fallSpeed * fallSpeed);
+		
+		var   dir = new Vector2(center.x, center.y) - new Vector2(shape.position.x, shape.position.y);
+		float rotation = -Mathf.Atan2(dir.y, dir.x);
+		shape.rotation = new Vector3(rotation * Mathf.Rad2Deg,               90,                  0);
+		main.startRotation = rotation;
+	}
+
+	#endregion
+
+	private void OnDrawGizmos() {
+		Gizmos.DrawSphere(rainSystem.shape.position, 0.1f);
+	}
+}
